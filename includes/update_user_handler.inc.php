8b48c16851752a8a7cc0c86cff364021eb2f0fbe
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2026-06-26 15:59:46 +0800

    __DIR__ in update user

--- includes/update_user_handler.inc.php
+++ includes/update_user_handler.inc.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     try {
         // import from this file
-        require_once "dbhost.inc.php";
+        require_once __DIR__ . "/dbhost.inc.php";
 
         // insert into DB
         // method 1
