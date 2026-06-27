3c8777338074244534ec937fdf98177255525d3b
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2026-06-27 22:25:31 +0800

    signup model- interact with Db. controller expose the function in signup.inc.php

---
+++ login_and_signup/signup_controller.inc.php
@@ -0,0 +1,44 @@
+<?php
+
+// to prevent type coercion
+// example: $x is int. cannot change it to string
+declare(strict_types=1);
+
+
+// validate empty inputs
+function is_input_empty(string $username, string $password, string $email)
+{
+
+    return (empty($username) || empty($password) || empty($email));
+}
+
+
+// validate email
+function is_email_valid(string $email)
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+
+// check if username is taken
+function is_username_taken(object $pdo, string $username)
+{
+
+    // get_username is function in signup_model.inc (interact with DB)
+    if (get_username($pdo, $username)) {
+        return true;
+    }
+    return false;
+}
+
+
+// check if email is taken
+function is_email_registered(object $pdo, string $email)
+{
+
+    // get_email is function in signup_model.inc (interact with DB)
+    if (get_email($pdo, $email)) {
+        return true;
+    }
+    return false;
+}
