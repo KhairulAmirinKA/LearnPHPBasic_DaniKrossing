698380f2d0525497e7a3478ea84e6d8a1b845270
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2026-06-28 16:12:41 +0800

    change session config

--- config/session_config.php
+++ config/session_config.php
@@ -26,22 +26,65 @@ session_set_cookie_params(
 session_start();
 
 
-if (!isset($_SESSION['last_regeneration'])) {
+if (isset($_SESSION['user_id'])) {
+
+    if (!isset($_SESSION['last_regeneration'])) {
+
+        regenerate_session_id_logged_in();
+    } else {
+        // 30 minit *60s
+        $interval = 60 * 30;
+
+        // lepasi certain time(30 minit), kena regenerate new session ID
+        if (time() - $_SESSION['last_regeneration'] >= $interval) {
+            regenerate_session_id_logged_in();
+        }
+    }
+} else {
+    if (!isset($_SESSION['last_regeneration'])) {
+
+        regenerate_session_id();
+    } else {
+
+        // 30 minit *60s
+        $interval = 60 * 30;
+
+        // lepasi certain time(30 minit), kena regenerate new session ID
+        if (time() - $_SESSION['last_regeneration'] >= $interval) {
+            regenerate_session_id();
+        }
+    }
+}
+
+
+// regenerate session ID
+function regenerate_session_id()
+{
 
     session_regenerate_id(true);
 
     // current time
     $_SESSION['last_regeneration'] = time();
-} 
+}
 
-else {
 
-    // 30 minit *60s
-    $interval = 60 * 30;
+function regenerate_session_id_logged_in()
+{
 
-    // lepasi certain time(30 minit), kena regenerate new session ID
-    if (time() - $_SESSION['last_regeneration'] >= $interval) {
-        session_regenerate_id(true);
-        $_SESSION['last_regeneration'] = time();
-    }
+    session_regenerate_id(true);
+
+
+    $userId = $_SESSION['user_id'];
+
+    // new session ID
+        $newSessionId = session_create_id();
+
+        // result[id] is user ID
+        $sessionId = $newSessionId . "_" . $userId;
+
+        session_id($sessionId);
+
+
+    // current time
+    $_SESSION['last_regeneration'] = time();
 }
